895c5c77eb235140f2b8028123c02f725d1c9baa
Hossein <djalicrt@gmail.com>
2021-11-20 19:22:08 +0330

    Show Title and Overview And Average IMDB Vote + Style of Search Box

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React , {useEffect , useState} from "react";
 import Movie from "./components/Movie";
 
-const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?api_key=db68f50c83657f5ef29885bef9520b12&language=en-US&sort_by=popularity.desc&page=1"
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?api_key=db68f50c83657f5ef29885bef9520b12&language=en-US&sort_by=popularity.desc&page=2"
 const IMG_API = "https://image.tmdb.org/t/p/w1200"
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=db68f50c83657f5ef29885bef9520b12&language=en-US&page=1"
 
@@ -17,11 +17,16 @@ function App() {
     })
   }, [])
 
-  return <div>
-    {movies.length > 0 && movies.map((movie) => (
-      <Movie key={movie.id} {...movie} />
-    ))}
-    </div>
+  return (
+  <><header>
+      <input className="search" type="search" placeholder="Search..." />
+    </header>
+    <div className="movie-container">
+        {movies.length > 0 && movies.map((movie) => (
+          <Movie key={movie.id} {...movie} />
+        ))}
+      </div></>
+    )
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,7 +5,15 @@ const IMG_API = "https://image.tmdb.org/t/p/w1280"
 
 const Movie = ( { title, poster_path, overview, vote_average } ) => (
     <div className="movie">
-        <img src={IMG_API + poster_path} />
+        <img src={IMG_API + poster_path} alt={title} />
+        <div className="movie-info">
+            <h3>{title}</h3>
+            <span>{vote_average}</span>
+        </div>
+        <div className="movie-over">
+            <h2>OverView:</h2>
+            <p>{overview}</p>
+        </div>
     </div>
 );
 
