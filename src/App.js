7de7f41692bda9d350765a7580c8cbfc3655f2db
Hossein <djalicrt@gmail.com>
2021-11-20 18:21:16 +0330

    Added Forgotten File!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useEffect , useState} from "react";
+import Movie from "./components/Movie";
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?api_key=db68f50c83657f5ef29885bef9520b12&language=en-US&sort_by=popularity.desc&page=1"
+const IMG_API = "https://image.tmdb.org/t/p/w1200"
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=db68f50c83657f5ef29885bef9520b12&language=en-US&page=1"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [ movies , setMovies ] = useState ([]);
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+    .then((res) => res.json())
+    .then ((data) => {
+      console.log(data);
+      setMovies(data.results);
+    })
+  }, [])
+
+  return <div>
+    {movies.length > 0 && movies.map((movie) => (
+      <Movie key={movie.id} {...movie} />
+    ))}
     </div>
-  );
 }
 
 export default App;
